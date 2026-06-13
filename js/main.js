c595f1a4764360a97ad9fa2bc4f19bcf30b11b20
CHANUKA ISURU SAMPATH <chanuchanuka60@gmail.com>
2026-06-13 23:52:23 +0530

    Update portfolio site content and layout

--- js/main.js
+++ js/main.js
@@ -3,6 +3,109 @@
 
 (function ($) {
 
+    function setBackground($element) {
+        if (!$element.length || $element.data('bg-loaded')) {
+            return;
+        }
+
+        var bg = $element.data('setbg');
+
+        if (bg) {
+            $element.css('background-image', 'url(' + bg + ')');
+            $element.data('bg-loaded', true);
+        }
+    }
+
+    function loadVisibleBackgrounds($scope) {
+        $scope.find('.set-bg').addBack('.set-bg').each(function () {
+            setBackground($(this));
+        });
+    }
+
+    function initPortfolioPagination() {
+        var $gallery = $('.portfolio__gallery');
+        var $pagination = $('.pagination__option');
+
+        if (!$gallery.length || !$pagination.length) {
+            return false;
+        }
+
+        var $items = $gallery.children('.mix');
+        var itemsPerPage = 12;
+        var activeFilter = '*';
+        var currentPage = 1;
+
+        function getFilteredItems() {
+            if (activeFilter === '*') {
+                return $items;
+            }
+
+            return $items.filter(activeFilter);
+        }
+
+        function renderPagination(totalPages) {
+            var paginationHtml = '<a href="#" class="arrow__pagination left__arrow"><span class="arrow_left"></span> Prev</a>';
+
+            for (var i = 1; i <= totalPages; i++) {
+                paginationHtml += '<a href="#" class="number__pagination" data-page="' + i + '">' + i + '</a>';
+            }
+
+            paginationHtml += '<a href="#" class="arrow__pagination right__arrow">Next <span class="arrow_right"></span></a>';
+            $pagination.html(paginationHtml).toggle(totalPages > 1);
+        }
+
+        function showPage(page) {
+            var $filteredItems = getFilteredItems();
+            var totalPages = Math.max(Math.ceil($filteredItems.length / itemsPerPage), 1);
+
+            currentPage = Math.min(Math.max(page, 1), totalPages);
+
+            var start = (currentPage - 1) * itemsPerPage;
+            var end = start + itemsPerPage;
+
+            $items.hide();
+            $filteredItems.slice(start, end).show();
+            loadVisibleBackgrounds($filteredItems.slice(start, end));
+
+            renderPagination(totalPages);
+            $pagination.find('[data-page="' + currentPage + '"]').addClass('active');
+            $pagination.find('.left__arrow').toggleClass('disabled', currentPage === 1);
+            $pagination.find('.right__arrow').toggleClass('disabled', currentPage === totalPages);
+        }
+
+        $('.portfolio__filter li').on('click', function () {
+            $('.portfolio__filter li').removeClass('active');
+            $(this).addClass('active');
+            activeFilter = $(this).data('filter');
+            showPage(1);
+        });
+
+        $pagination.on('click', 'a', function (event) {
+            event.preventDefault();
+
+            var $link = $(this);
+
+            if ($link.hasClass('disabled') || $link.hasClass('active')) {
+                return;
+            }
+
+            if ($link.hasClass('left__arrow')) {
+                showPage(currentPage - 1);
+            } else if ($link.hasClass('right__arrow')) {
+                showPage(currentPage + 1);
+            } else {
+                showPage(parseInt($link.data('page'), 10));
+            }
+
+            $('html, body').animate({
+                scrollTop: $('.portfolio').offset().top - 90
+            }, 250);
+        });
+
+        showPage(1);
+        return true;
+    }
+
     /*------------------
         Preloader
     --------------------*/
@@ -13,11 +116,11 @@
         /*------------------
             Portfolio filter
         --------------------*/
-        $('.portfolio__filter li').on('click', function () {
-            $('.portfolio__filter li').removeClass('active');
-            $(this).addClass('active');
-        });
-        if ($('.portfolio__gallery').length > 0) {
+        if (!initPortfolioPagination() && $('.portfolio__gallery').length > 0) {
+            $('.portfolio__filter li').on('click', function () {
+                $('.portfolio__filter li').removeClass('active');
+                $(this).addClass('active');
+            });
             var containerEl = document.querySelector('.portfolio__gallery');
             var mixer = mixitup(containerEl);
         }
@@ -26,9 +129,8 @@
     /*------------------
         Background Set
     --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
+    $('.set-bg').not('.portfolio__gallery .set-bg').each(function () {
+        setBackground($(this));
     });
 
     //Masonary
@@ -160,13 +262,18 @@
         Counter
     --------------------*/
     $('.counter_num').each(function () {
+        var targetNumber = $(this).text();
+
         $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
+            Counter: targetNumber
         }, {
             duration: 4000,
             easing: 'swing',
             step: function (now) {
                 $(this).text(Math.ceil(now));
+            },
+            complete: function () {
+                $(this).text(targetNumber);
             }
         });
     });
